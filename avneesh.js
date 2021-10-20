47331f44576eda8aba277fab5e20d8e393e25287
adityarungta2048 <71099075+AdityaRungta2048@users.noreply.github.com>
2021-10-20 12:45:25 +0530

    Update avneesh.js

--- avneesh.js
+++ avneesh.js
@@ -2,7 +2,7 @@ student = {
     firstName: 'Avneesh',
     lastName: 'Singh',
     studentAge: 20,
-    enrollmentNo: 'A2305218462',
+    enrollmentNo: 'A2305218457',
     studentInfo: function(){
         return this.firstName + " " + this.lastName + "\n" + this.enrollmentNo + "\n" + this.studentAge;
     }
